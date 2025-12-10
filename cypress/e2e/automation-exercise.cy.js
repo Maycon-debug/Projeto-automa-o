f0a9dffa4933501d9a8e39053cd123a8279e1bfe
Maycon-debug <mayconykarus@gmail.com>
2025-12-09 23:15:08 -0300

    Atualizado_funcionando

--- cypress/e2e/automation-exercise.cy.js
+++ cypress/e2e/automation-exercise.cy.js
@@ -1,9 +1,11 @@
 describe('Automation Exercise', () => {
-    // Gerar email único para evitar conflitos
-    const timestamp = Date.now()
-    const email = `teste${timestamp}@teste.com`
     const senha = '@12345678'
     const nome = 'Maycon'
+
+    // Função para gerar email único
+    const gerarEmail = () => {
+        return `teste${Date.now()}${Math.random().toString(36).substring(7)}@teste.com`
+    }
 
     beforeEach(() => {
         // Visitar o site antes de cada teste
@@ -11,27 +13,38 @@ describe('Automation Exercise', () => {
     })
 
     it('TC01: Cadastro do Usuario', () => {
+        const email = gerarEmail()
         // clicar em Signup/Login
-        cy.get('a[href="/login"]').click()
+        cy.contains('Signup / Login').click()
         // preencher o formulario de cadastro
         cy.get('input[data-qa="signup-name"]').type(nome)
         // digite email único
         cy.get('input[data-qa="signup-email"]').type(email)
         // clicar em Signup button
         cy.get('button[data-qa="signup-button"]').click()
+        // aguardar carregamento do formulário
+        cy.get('form').should('be.visible')
         // preencher informações do formulário de cadastro
-        cy.get('input[data-qa="password"]').type(senha)
-        cy.get('input[data-qa="first-name"]').type('Maycon')
-        cy.get('input[data-qa="last-name"]').type('Santos')
-        cy.get('input[data-qa="address"]').type('Rua Teste, 123')
-        cy.get('input[data-qa="state"]').type('São Paulo')
-        cy.get('input[data-qa="city"]').type('São Paulo')
-        cy.get('input[data-qa="zipcode"]').type('01234567')
-        cy.get('input[data-qa="mobile-number"]').type('11999999999')
+        cy.get('#id_gender1').check() // Title: Mr
+        cy.get('#password').type(senha)
+        cy.get('#days').select('15')
+        cy.get('#months').select('5')
+        cy.get('#years').select('1990')
+        cy.get('#newsletter').check()
+        cy.get('#optin').check()
+        cy.get('#first_name').type('Maycon')
+        cy.get('#last_name').type('Santos')
+        cy.get('#company').type('Empresa Teste')
+        cy.get('#address1').type('Rua Teste, 123')
+        cy.get('#address2').type('Apto 45')
+        cy.get('#country').select('United States')
+        cy.get('#state').type('São Paulo')
+        cy.get('#city').type('São Paulo')
+        cy.get('#zipcode').type('01234567')
+        cy.get('#mobile_number').type('11999999999')
         // clicar em Create Account button
         cy.get('button[data-qa="create-account"]').click()
         // verificar se a mensagem de sucesso foi exibida
-        cy.get('h2[data-qa="account-created"]').should('be.visible')
         cy.contains('Account Created!').should('be.visible')
         // clicar em Continue
         cy.get('a[data-qa="continue-button"]').click()
@@ -39,31 +52,39 @@ describe('Automation Exercise', () => {
         cy.contains('Logged in as').should('be.visible')
         cy.contains(nome).should('be.visible')
         // clicar em Logout
-        cy.get('a[href="/logout"]').click()
+        cy.contains('Logout').click()
         // verificar se foi deslogado
         cy.url().should('include', '/login')
     })
 
     it('TC02: Login do Usuario', () => {
+        const email = gerarEmail()
         // Primeiro criar uma conta para testar o login
-        cy.get('a[href="/login"]').click()
+        cy.contains('Signup / Login').click()
         cy.get('input[data-qa="signup-name"]').type(nome)
         cy.get('input[data-qa="signup-email"]').type(email)
         cy.get('button[data-qa="signup-button"]').click()
-        cy.get('input[data-qa="password"]').type(senha)
-        cy.get('input[data-qa="first-name"]').type('Maycon')
-        cy.get('input[data-qa="last-name"]').type('Santos')
-        cy.get('input[data-qa="address"]').type('Rua Teste, 123')
-        cy.get('input[data-qa="state"]').type('São Paulo')
-        cy.get('input[data-qa="city"]').type('São Paulo')
-        cy.get('input[data-qa="zipcode"]').type('01234567')
-        cy.get('input[data-qa="mobile-number"]').type('11999999999')
+        cy.get('form').should('be.visible')
+        cy.get('#id_gender1').check()
+        cy.get('#password').type(senha)
+        cy.get('#days').select('15')
+        cy.get('#months').select('5')
+        cy.get('#years').select('1990')
+        cy.get('#first_name').type('Maycon')
+        cy.get('#last_name').type('Santos')
+        cy.get('#company').type('Empresa Teste')
+        cy.get('#address1').type('Rua Teste, 123')
+        cy.get('#state').type('São Paulo')
+        cy.get('#city').type('São Paulo')
+        cy.get('#zipcode').type('01234567')
+        cy.get('#mobile_number').type('11999999999')
         cy.get('button[data-qa="create-account"]').click()
+        cy.contains('Account Created!').should('be.visible')
         cy.get('a[data-qa="continue-button"]').click()
-        cy.get('a[href="/logout"]').click()
+        cy.contains('Logout').click()
 
         // Agora testar o login
-        cy.get('a[href="/login"]').click()
+        cy.contains('Signup / Login').click()
         // preencher email e senha
         cy.get('input[data-qa="login-email"]').type(email)
         cy.get('input[data-qa="login-password"]').type(senha)
@@ -79,17 +100,18 @@ describe('Automation Exercise', () => {
         cy.get('a[href="/products"]').click()
         // Aguardar carregamento dos produtos
         cy.get('.features_items').should('be.visible')
-        // Clicar no primeiro produto
+        // Clicar no primeiro produto (hover e depois clicar em View Product)
+        cy.get('.product-image-wrapper').first().trigger('mouseover')
         cy.get('.product-image-wrapper').first().within(() => {
-            cy.get('a').first().click()
+            cy.contains('View Product').click()
         })
         // Adicionar ao carrinho
-        cy.get('button[type="button"]').contains('Add to cart').click()
+        cy.contains('Add to cart').click()
         // Verificar modal de confirmação
         cy.get('#cartModal').should('be.visible')
         cy.contains('Your product has been added to cart').should('be.visible')
         // Clicar em View Cart
-        cy.get('a[href="/view_cart"]').contains('View Cart').click()
+        cy.contains('View Cart').click()
         // Verificar se o produto está no carrinho
         cy.url().should('include', '/view_cart')
         cy.get('.cart_info').should('be.visible')
@@ -99,11 +121,12 @@ describe('Automation Exercise', () => {
         // Adicionar produto ao carrinho
         cy.get('a[href="/products"]').click()
         cy.get('.features_items').should('be.visible')
+        cy.get('.product-image-wrapper').first().trigger('mouseover')
         cy.get('.product-image-wrapper').first().within(() => {
-            cy.get('a').first().click()
+            cy.contains('View Product').click()
         })
-        cy.get('button[type="button"]').contains('Add to cart').click()
-        cy.get('a[href="/view_cart"]').contains('View Cart').click()
+        cy.contains('Add to cart').click()
+        cy.contains('View Cart').click()
         
         // Verificar elementos do carrinho
         cy.get('.cart_info').should('be.visible')
@@ -116,41 +139,54 @@ describe('Automation Exercise', () => {
     })
 
     it('TC05: Finalizar Compra', () => {
+        const email = gerarEmail()
         // Primeiro fazer login
-        cy.get('a[href="/login"]').click()
+        cy.contains('Signup / Login').click()
         cy.get('input[data-qa="signup-name"]').type(nome)
         cy.get('input[data-qa="signup-email"]').type(email)
         cy.get('button[data-qa="signup-button"]').click()
-        cy.get('input[data-qa="password"]').type(senha)
-        cy.get('input[data-qa="first-name"]').type('Maycon')
-        cy.get('input[data-qa="last-name"]').type('Santos')
-        cy.get('input[data-qa="address"]').type('Rua Teste, 123')
-        cy.get('input[data-qa="state"]').type('São Paulo')
-        cy.get('input[data-qa="city"]').type('São Paulo')
-        cy.get('input[data-qa="zipcode"]').type('01234567')
-        cy.get('input[data-qa="mobile-number"]').type('11999999999')
+        cy.get('form').should('be.visible')
+        cy.get('#id_gender1').check()
+        cy.get('#password').type(senha)
+        cy.get('#days').select('15')
+        cy.get('#months').select('5')
+        cy.get('#years').select('1990')
+        cy.get('#first_name').type('Maycon')
+        cy.get('#last_name').type('Santos')
+        cy.get('#company').type('Empresa Teste')
+        cy.get('#address1').type('Rua Teste, 123')
+        cy.get('#state').type('São Paulo')
+        cy.get('#city').type('São Paulo')
+        cy.get('#zipcode').type('01234567')
+        cy.get('#mobile_number').type('11999999999')
         cy.get('button[data-qa="create-account"]').click()
+        cy.contains('Account Created!').should('be.visible')
         cy.get('a[data-qa="continue-button"]').click()
 
         // Adicionar produto ao carrinho
         cy.get('a[href="/products"]').click()
         cy.get('.features_items').should('be.visible')
+        cy.get('.product-image-wrapper').first().trigger('mouseover')
         cy.get('.product-image-wrapper').first().within(() => {
-            cy.get('a').first().click()
+            cy.contains('View Product').click()
         })
-        cy.get('button[type="button"]').contains('Add to cart').click()
-        cy.get('a[href="/view_cart"]').contains('View Cart').click()
+        cy.contains('Add to cart').click()
+        cy.contains('View Cart').click()
         
         // Proceder para checkout
         cy.contains('Proceed To Checkout').click()
-        // Verificar endereço de entrega
-        cy.get('.address_delivery').should('be.visible')
+        // Verificar se está na página de checkout
+        cy.url().should('include', '/checkout')
         // Verificar resumo do pedido
         cy.get('.cart_info').should('be.visible')
-        // Adicionar comentário
-        cy.get('textarea[name="message"]').type('Pedido de teste automatizado')
+        // Adicionar comentário (se o campo existir)
+        cy.get('body').then(($body) => {
+            if ($body.find('textarea[name="message"]').length > 0) {
+                cy.get('textarea[name="message"]').type('Pedido de teste automatizado')
+            }
+        })
         // Finalizar pedido
-        cy.get('a[href="/payment"]').click()
+        cy.contains('Place Order').click()
         // Preencher dados do cartão (dados de teste)
         cy.get('input[name="name_on_card"]').type('Maycon Santos')
         cy.get('input[name="card_number"]').type('1234567890123456')
@@ -158,7 +194,7 @@ describe('Automation Exercise', () => {
         cy.get('input[name="expiry_month"]').type('12')
         cy.get('input[name="expiry_year"]').type('2025')
         // Confirmar pagamento
-        cy.get('button[data-qa="pay-button"]').click()
+        cy.get('#submit').click()
         // Verificar mensagem de sucesso
         cy.contains('Order Placed!').should('be.visible')
         cy.contains('Congratulations! Your order has been confirmed').should('be.visible')
